151a1801afe69d2b54cc5b2954a12437ee78d647
Skia <fjacquet@tetrane.com>
2020-07-08 10:32:53 +0200

    Add support for `data-requires-python`
    This patch improves support for PEP 503 by adding the optional attribute
    `data-requires-python` when the info is available.
    See #296 for more details.

--- pypiserver/core.py
+++ pypiserver/core.py
@@ -206,6 +206,7 @@ class PkgFile(object):
                  'pkgname',
                  'version',
                  'parsed_version',
+                 '_requires_python',
                  'replaces']
 
     def __init__(self, pkgname, version, fn=None, root=None, relfn=None, replaces=None):
@@ -234,6 +235,23 @@ class PkgFile(object):
             else:
                 self._fname_and_hash = self.relfn_unix
         return self._fname_and_hash
+
+    def requires_python(self):
+        if not hasattr(self, '_requires_python'):
+            try:
+                from zipfile import ZipFile
+                from email.parser import Parser
+                with ZipFile(self.fn, 'r') as wheel:
+                    name = [e for e in wheel.namelist() if
+                            e.endswith(".dist-info/METADATA") or
+                            e.endswith("/PKG-INFO")][0]
+                    wheel_metadata = wheel.read(name)
+                    parser = Parser()
+                    metadata = parser.parsestr(wheel_metadata.decode("utf-8"))
+                    self._requires_python = metadata["Requires-Python"]
+            except Exception as e:
+                self._requires_python = ''
+        return self._requires_python
 
 
 def _listdir(root):
